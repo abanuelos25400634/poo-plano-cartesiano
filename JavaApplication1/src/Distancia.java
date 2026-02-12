2020c60514e91dd29e602ff21001d474c1ce0136
unknown <axzabanuelosam@ittepic.edu.mx>
2026-02-11 20:27:30 -0700

    feat: implementa clase Distancia con calculo euclidiano

--- JavaApplication1/src/Distancia.java
+++ JavaApplication1/src/Distancia.java
@@ -2,7 +2,37 @@
 public class Distancia 
 {
     //Esta clase tiene 2 atributos de tipo punto
+    private Punto puntoA;
+    private Punto puntoB;
+    private double distancia;
     
+    public Distancia(Punto puntoA,Punto puntoB)
+    {
+        this.puntoA = puntoA;
+        this.puntoB = puntoB;
+        calcular();
+    }
+    
+    
+    private void calcular()
+    {
+        double dx = puntoB.getX() - puntoA.getX();
+        double dy = puntoB.getY() - puntoA.getY();
+        this.distancia = Math.sqrt(dx*dx+dy*dy);
+        
+    }
+    
+    
+    public double getDistancia()
+    {
+        return distancia;
+    }
+    
+    @Override
+    public String toString()
+    {
+    return "Distancia entre "+this.puntoA +" y "+ this.puntoB +" = "+ String.format("%.4f", this.distancia);
+    }
     
     
 }
